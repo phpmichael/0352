a8a450e3f345d5dd4f3cf213b6a9a3a7b431f97a
phpmichael <php.michael@gmail.com>
2014-01-09 21:46:26 +0200

    include lightbox minified

--- application/views/inc/js-lightbox.php
+++ application/views/inc/js-lightbox.php
@@ -1,10 +1,9 @@
-<?=include_css($BC->_getFolder('js').'jquery/lightbox/css/jquery.lightbox-0.5.css')?>
+<?=include_minified($BC->_getFolder('js').'jquery/lightbox/css/jquery.lightbox-0.5.css','css')?>
 <script type="text/javascript">var LightBoxPath = "<?=base_url().$BC->_getFolder('js')?>jquery/lightbox/";</script> 
-<?=include_js($BC->_getFolder('js').'jquery/lightbox/js/jquery.lightbox-0.5.js')?>
+<?=include_minified($BC->_getFolder('js').'jquery/lightbox/js/jquery.lightbox-0.5.js','js')?>
 <script type="text/javascript">
 //<![CDATA[
-$j(function() 
-{
+$j(function(){
 	$j('a.product-image, a.lightbox, a[rel=lightbox]').lightBox();
 });
 //]]>
