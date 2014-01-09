525d023222e7cf320c6d971591820f08c4c055d4
phpmichael <php.michael@gmail.com>
2014-01-10 00:14:13 +0200

    small fixes

--- application/views/themes/navchalka/main.php
+++ application/views/themes/navchalka/main.php
@@ -5,7 +5,7 @@ $BC->lang->load('calendar');
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
+	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	<title><?=$BC->_getPageTitle()?></title>
