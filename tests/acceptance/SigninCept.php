286d0fa92c5f9e2f9ea90930a27f531838b5514d
phpmichael <php.michael@gmail.com>
2014-01-12 11:13:38 +0200

    Delete SigninCept.php

--- tests/acceptance/SigninCept.php
+++
@@ -1,9 +0,0 @@
-<?php
-$I = new WebGuy($scenario);
-$I->wantTo('log in as regular user');
-$I->amOnPage('customers/signin');
-$I->see('Authorization');
-$I->fillField('email','php.michael@gmail.com');
-$I->fillField('password','codemaster7');
-$I->click('Login');
-$I->see('Logout');
