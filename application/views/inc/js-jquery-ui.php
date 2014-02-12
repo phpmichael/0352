9705367c6e69fc30a5b84c4148a886b96286a18e
phpmichael <php.michael@gmail.com>
2014-02-12 12:40:27 +0200

    added local version of new jquery and jquery ui

--- application/views/inc/js-jquery-ui.php
+++ application/views/inc/js-jquery-ui.php
@@ -1,4 +1,4 @@
-<?//=include_css($BC->_getFolder('js').'jquery/ui/css/flick/jquery-ui-1.8.15.custom.css')?>
-<?//=include_js($BC->_getFolder('js').'jquery/ui/js/jquery-ui-1.8.15.custom.min.js')?>
+<?//=include_css($BC->_getFolder('js').'jquery/ui/1.10.4/themes/smoothness/jquery-ui.min.css')?>
+<?//=include_js($BC->_getFolder('js').'jquery/ui/1.10.4/jquery-ui.min.js')?>
 <link href="http://code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css" rel="stylesheet" />
 <script src="http://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
