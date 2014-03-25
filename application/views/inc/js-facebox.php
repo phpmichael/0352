8a6010d9faf2e06ca760da32b81e58164c780936
phpmichael <php.michael@gmail.com>
2014-03-25 19:39:49 +0200

    include facebox.css minified

--- application/views/inc/js-facebox.php
+++ application/views/inc/js-facebox.php
@@ -1,4 +1,4 @@
-<?=include_css($BC->_getFolder('js').'jquery/facebox/facebox.css')?>
+<?=include_minified($BC->_getFolder('js').'jquery/facebox/facebox.css','css')?>
 <script>var FaceBoxPath = "<?=base_url().$BC->_getFolder('js')?>jquery/facebox/";</script>
 <?=include_minified($BC->_getFolder('js').'jquery/facebox/facebox.js','js')?>
 <?=include_minified($BC->_getFolder('js').'jquery/facebox/init.js','inline_js')?>
