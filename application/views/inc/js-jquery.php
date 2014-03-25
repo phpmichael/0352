6270c6e20a2254213cd728b17fe575f05c07bfb9
phpmichael <php.michael@gmail.com>
2014-03-25 22:39:58 +0200

    Commented jquery migration script

--- application/views/inc/js-jquery.php
+++ application/views/inc/js-jquery.php
@@ -1,2 +1,3 @@
 <?=include_js($BC->_getFolder('js').'jquery/jquery-1.11.0.min.js')?>
+<?//=include_js($BC->_getFolder('js').'jquery/jquery-migrate-1.2.1.min.js')?>
 <script>$j = jQuery.noConflict();</script>
