4cff66a4785a831421d746ba833aec6f176b2da2
phpmichael <php.michael@gmail.com>
2015-01-15 16:44:10 +0200

    ZNO theme: fix for wishlist

--- application/views/themes/zno/wishlist/index.php
+++ application/views/themes/zno/wishlist/index.php
@@ -1,22 +1,20 @@
 <h1><?=$BC->_getPageTitle()?></h1>
 
 <?if( $wishlist ):?>
-
-<table class="table">
-    <tr class="rowH">
-        <th><?=language('thing_name')?></th>
-        <th><?=language('price')?></th>
-        <th><?=language('delete')?></th>
-    </tr>
-    <?foreach ($wishlist as $key=>$row):?>
-    <tr class="row<?if($key%2):?>A<?else:?>B<?endif?>">
-        <td><?=anchor_base('products/view/'.$row['product_id'],$row['product']['name'])?></td>
-        <td><?=exchange($row['product']['price'])?></td>
-        <td><?=anchor($BC->_getBaseURI().'/remove/'.$row['id'],language('delete'))?></td>
-    </tr>
-    <?endforeach;?>
-</table>
-
+    <table class="table">
+        <tr class="rowH">
+            <th><?=language('thing_name')?></th>
+            <th><?=language('price')?></th>
+            <th><?=language('delete')?></th>
+        </tr>
+        <?foreach ($wishlist as $key=>$row):?>
+        <tr class="row<?if($key%2):?>A<?else:?>B<?endif?>">
+            <td><?=anchor_base('book/'.$row['product']['slug'],$row['product']['name'],"class='product_name'")?></td>
+            <td><?=exchange($row['product']['price'])?></td>
+            <td><?=anchor($BC->_getBaseURI().'/remove/'.$row['id'],language('delete'))?></td>
+        </tr>
+        <?endforeach;?>
+    </table>
 <?else:?>
     <h2><?=language('your_wishlist_is_empty')?></h2>
 <?endif?>
