8808eea85514e00409077085516d4ec214f4fc3f
phpmichael <php.michael@gmail.com>
2015-01-15 16:38:54 +0200

    ZNO theme: added wishlist

--- application/views/themes/zno/customers/index.php
+++ application/views/themes/zno/customers/index.php
@@ -4,6 +4,7 @@
     <ul>
 		<li><?=anchor_base('customers/editinfo', language('edit_my_info'))?></li>
 		<li><?=anchor_base('orders/history', language('my_orders_history'))?></li>
+		<li><?=anchor_base('wishlist', language('my_wishlist'))?></li>
 		<li><?=anchor_base('customers/signout', language('logout'))?></li>
 	</ul>
 </div>
---
+++ application/views/themes/zno/wishlist/index.php
@@ -0,0 +1,22 @@
+<h1><?=$BC->_getPageTitle()?></h1>
+
+<?if( $wishlist ):?>
+
+<table class="table">
+    <tr class="rowH">
+        <th><?=language('thing_name')?></th>
+        <th><?=language('price')?></th>
+        <th><?=language('delete')?></th>
+    </tr>
+    <?foreach ($wishlist as $key=>$row):?>
+    <tr class="row<?if($key%2):?>A<?else:?>B<?endif?>">
+        <td><?=anchor_base('products/view/'.$row['product_id'],$row['product']['name'])?></td>
+        <td><?=exchange($row['product']['price'])?></td>
+        <td><?=anchor($BC->_getBaseURI().'/remove/'.$row['id'],language('delete'))?></td>
+    </tr>
+    <?endforeach;?>
+</table>
+
+<?else:?>
+    <h2><?=language('your_wishlist_is_empty')?></h2>
+<?endif?>
