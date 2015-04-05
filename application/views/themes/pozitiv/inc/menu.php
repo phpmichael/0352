42f8efb95564b62db89984043e69ae29fcf31f22
phpmichael <php.michael@gmail.com>
2015-04-05 13:24:14 +0300

    pozitive theme: changes to menu

--- application/views/themes/pozitiv/inc/menu.php
+++ application/views/themes/pozitiv/inc/menu.php
@@ -2,7 +2,6 @@
 <nav>
 	<?=get_categories_tree('photos',0,@$search_category_id);?>
 	<ul>
-	    <li><a href="<?=site_url($BC->_getBaseUrl().'videos')?>"><?=language('videos')?></a></li>
 	    <li><a href="<?=site_url($BC->_getBaseUrl().'contact_us')?>"><?=language('contacts')?></a></li>
 	</ul>
 </nav>
