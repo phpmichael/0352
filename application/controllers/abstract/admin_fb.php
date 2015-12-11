bf7c0d77afcfd2f69bff71d4ace3f9248d8b8c8d
phpmichael <php.michael@gmail.com>
2015-12-11 12:54:29 +0200

    Added: @property Formbuilder_model $model

--- application/controllers/abstract/admin_fb.php
+++ application/controllers/abstract/admin_fb.php
@@ -8,6 +8,7 @@ require_once(APPPATH.'controllers/abstract/admin.php');
  * @author Michael Kovalskiy
  * @version 2012
  * @access public
+ * @property Formbuilder_model $model
  */
 class Admin_fb extends Admin 
 {
