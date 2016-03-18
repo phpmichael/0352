60e1c8a1b21c478fceb9a4a15c59cf62579a471a
phpmichael <php.michael@gmail.com>
2016-03-18 20:54:48 +0200

    zno theme: moved social icons in separate file

--- application/views/themes/zno/inc/left-sidebar.php
+++ application/views/themes/zno/inc/left-sidebar.php
@@ -5,13 +5,7 @@
     <?load_theme_view('inc/box-recent-articles')?>
 <?endif?>
 
-<h2><?=language('follow_us')?></h2>
-<div class="row-fluid">
-    <a class="social-icon social-google" href="#"></a>
-    <a class="social-icon social-twitter" href="#"></a>
-    <a class="social-icon social-vkontakte" href="#"></a>
-    <a class="social-icon social-odnoklasniki" href="#"></a>
-</div>
+
 
 <?if (($BC->is_home_page())):?>
     <h2>Графік ЗНО-2015</h2>
---
+++ application/views/themes/zno/inc/social-icons.php
@@ -0,0 +1,7 @@
+<h2><?=language('follow_us')?></h2>
+<div class="row-fluid">
+    <a class="social-icon social-google" href="#"></a>
+    <a class="social-icon social-twitter" href="#"></a>
+    <a class="social-icon social-vkontakte" href="#"></a>
+    <a class="social-icon social-odnoklasniki" href="#"></a>
+</div>
