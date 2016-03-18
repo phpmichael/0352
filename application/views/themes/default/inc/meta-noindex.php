b56ac1c7464949bdeec3a54fc91a244554a5da69
phpmichael <php.michael@gmail.com>
2016-03-18 23:05:26 +0200

    zno theme: noindex for ducuments page, but should still follow for download

--- application/views/themes/default/inc/meta-noindex.php
+++ application/views/themes/default/inc/meta-noindex.php
@@ -2,6 +2,8 @@
     $BC->_getController() == 'customers'
     ||
     in_array($BC->_getController(), array('articles', 'news')) && in_array($BC->_getMethod(), array('index', 'tag'))
+    ||
+    $BC->_getController() == 'documents' && in_array($BC->_getMethod(), array('index', 'search'))
 ):?>
     <meta name="robots" content="noindex">
 <?endif?>
