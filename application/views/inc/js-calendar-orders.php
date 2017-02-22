162b716bccac355b81818b6bf91c2ed6796c1551
phpmichael <php.michael@gmail.com>
2017-02-23 01:27:52 +0200

    Display orders like calendar: removed agenda option

--- application/views/inc/js-calendar-orders.php
+++ application/views/inc/js-calendar-orders.php
@@ -9,7 +9,6 @@
             firstDay: 1, //start week from Monday
             weekMode: 'liquid', //change default view of 6 row always
             timeFormat: 'H:mm', // uppercase H for 24-hour clock
-            axisFormat: 'H:mm', // for agenda view (left)
             events: "<?=site_url($BC->_getBaseURI().'/calendar')?>",//load events
             eventClick: function(calEvent) {
                 location.href = "<?=site_url($BC->_getBaseURI().'/edit/id/desc/0/')?>/"+calEvent.id ;
