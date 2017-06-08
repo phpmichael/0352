67c346ffb8f30a0588190417552634e1b325dffd
phpmichael <php.michael@gmail.com>
2017-06-09 01:02:33 +0300

    NovaPoshta: send button ability

--- application/views/themes/admin/default/orders/shipping/buttons.php
+++ application/views/themes/admin/default/orders/shipping/buttons.php
@@ -1,4 +1,6 @@
 <?$orders_customer_info = $BC->orders_model->getOrderCustomerInfo($orders_customer_info_id);?>
 <?if($orders_customer_info['shipping_type']=='ukrposhta'):?>
     <?=anchor_base("shipping/printUrkPost/".$orders_customer_info_id, 'Друк квитанції для Укрпошти', 'target="_blank"' )?>
+<?elseif($orders_customer_info['shipping_type']=='novaposhta'):?>
+    <?=anchor_base("shipping/novaposhtaSend/".$id, 'Надіслати в Нову Пошту')?>
 <?endif?>
