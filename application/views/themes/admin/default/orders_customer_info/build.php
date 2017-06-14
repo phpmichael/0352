261ab4f58e9a2d24494ae818834783b26a60e2e1
phpmichael <php.michael@gmail.com>
2017-06-14 15:40:58 +0300

    small fixes in URLS

--- application/views/themes/admin/default/orders_customer_info/build.php
+++ application/views/themes/admin/default/orders_customer_info/build.php
@@ -1,5 +1,5 @@
 <?if($order = $BC->orders_model->getOneByUnique('orders_customer_info_id', $data_key)):?>
-   <h2><?=anchor($BC->_getBaseURL().'/orders/edit/id/desc/0/'.$order['id'],'Order #'.$order['id'])?></h2>
+   <h2><?=anchor($BC->_getBaseURL().'orders/edit/id/desc/0/'.$order['id'],'Order #'.$order['id'])?></h2>
 <?endif;?>
 
 <?fb_form($form_id,$data_key)?>
