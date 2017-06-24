5803445e67fe534a1d1b11ffe62eadc7fb878bde
phpmichael <php.michael@gmail.com>
2017-06-24 18:40:04 +0300

    Novaposhta: print

--- application/views/themes/admin/default/orders/shipping/buttons.php
+++ application/views/themes/admin/default/orders/shipping/buttons.php
@@ -3,4 +3,7 @@
     <?=anchor_base("shipping/printUrkPost/".$orders_customer_info_id, 'Друк квитанції для Укрпошти', 'target="_blank"' )?>
 <?elseif($orders_customer_info['shipping_type']=='novaposhta'):?>
     <?=anchor_base("shipping/novaposhtaSend/".$id, 'Надіслати в Нову Пошту')?>
+    <?if($orders_customer_info['doc_number']):?>
+        | <a href="https://my.novaposhta.ua/orders/printDocument/orders[]/<?=$orders_customer_info['doc_number']?>/type/pdf/apiKey/<?=@$BC->settings_model['novaposhta_apiKey']?>" target="_blank">Друк накладної</a>
+    <?endif?>
 <?endif?>
