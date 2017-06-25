1bd0726cdfc600707f37934970904a2dcbe3dfb9
phpmichael <php.michael@gmail.com>
2017-06-25 15:30:05 +0300

    Ukrposhta:  Send E-Invoice Number

--- application/views/themes/admin/default/orders/shipping/buttons.php
+++ application/views/themes/admin/default/orders/shipping/buttons.php
@@ -1,6 +1,11 @@
 <?$orders_customer_info = $BC->orders_model->getOrderCustomerInfo($orders_customer_info_id);?>
 <?if($orders_customer_info['shipping_type']=='ukrposhta'):?>
     <?=anchor_base("shipping/printUrkPost/".$orders_customer_info_id, 'Друк квитанції для Укрпошти', 'target="_blank"' )?>
+    <?if($orders_customer_info['doc_number']):?>
+        <?if(!$orders_customer_info['doc_number_sent']):?>
+            | <a href="javascript: if(confirm('<?=language("are_you_sure")?>')) location.href='<?=site_url($BC->_getBaseURL()."shipping/sendEN/".$id)?>'">Надіслати ЕН</a>
+        <?endif?>
+    <?endif?>
 <?elseif($orders_customer_info['shipping_type']=='novaposhta'):?>
     <?=anchor_base("shipping/novaposhtaSend/".$id, 'Надіслати в Нову Пошту')?>
     <?if($orders_customer_info['doc_number']):?>
