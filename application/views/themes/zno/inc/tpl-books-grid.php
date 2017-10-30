5b9df58ecca8fbaffbcaf8e4e255d283a4b52111
Michael Kovalskiy <php.michael@gmail.com>
2017-10-31 00:23:24 +0200

    ZNO: remove wishlist button

--- application/views/themes/zno/inc/tpl-books-grid.php
+++ application/views/themes/zno/inc/tpl-books-grid.php
@@ -37,9 +37,6 @@
 
                                     <div class="pull-left">
                                         <?=form_button('add_to_cart','<i class="icon-shopping-cart"></i>'.language('buy'),"class='btn add-product-submit'")?>
-                                        <?if(!is_product_in_wishlist($row->data_key)):?>
-                                            <?=form_button('add_to_wishlist','<i class="icon-star"></i>',"class='add-to-wishlist btn' id='add-to-wishlist-".$row->data_key."'")?>
-                                        <?endif?>
                                     </div>
                                 <?else:?>
                                     <?=language('not_in_stock')?>
