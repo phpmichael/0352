4f02b687a21b316429fe7edba862cc7cea0ba501
Michael Kovalskiy <php.michael@gmail.com>
2018-08-03 21:17:47 +0300

    Ukrposhta: send and get sticker links

--- application/views/themes/admin/default/orders/shipping/buttons.php
+++ application/views/themes/admin/default/orders/shipping/buttons.php
@@ -2,9 +2,12 @@
 <?if($orders_customer_info['shipping_type']=='ukrposhta'):?>
     <?=anchor_base("shipping/printUrkPost/".$orders_customer_info_id, 'Друк квитанції для Укрпошти', 'target="_blank"' )?>
     <?if($orders_customer_info['doc_number']):?>
+        | <?=anchor_base("shipping/ukrposhtaSticker/".$orders_customer_info['doc_number'], 'Стікер Укрпошти', 'target="_blank"')?>
         <?if(!$orders_customer_info['doc_number_sent']):?>
             | <a href="javascript: if(confirm('<?=language("are_you_sure")?>')) location.href='<?=site_url($BC->_getBaseURL()."shipping/sendEN/".$id)?>'">Надіслати ЕН</a>
         <?endif?>
+    <?else:?>
+        | <?=anchor_base("shipping/ukrposhtaSend/".$id, 'Надіслати до Укрпошти' )?>
     <?endif?>
 <?elseif($orders_customer_info['shipping_type']=='novaposhta'):?>
     <?=anchor_base("shipping/novaposhtaSend/".$id, 'Надіслати в Нову Пошту')?>
