9537308ba9eda6f87756e0b43d97882f83b983e0
Michael Kovalskiy <php.michael@gmail.com>
2021-01-14 15:20:35 +0200

    Quiz: fix height/float for answers images

--- application/views/themes/default/quiz/inc/answers-images.php
+++ application/views/themes/default/quiz/inc/answers-images.php
@@ -1,10 +1,20 @@
 <!-- Answer images: Start -->
+<?php
+//make layers the same height for correct float
+$answerImagesHeight = array();
+foreach ($answers as $aIndex=>$answer) {
+    if($answer['image']) {
+        list($width, $height) = getimagesize($BC->_getFolder('images') . 'data/b/quiz/' . $answer['image']);
+        $answerImagesHeight[] = $height;
+    }
+}
+$imgLayerHeight = max($answerImagesHeight)+20;
+?>
 <?foreach ($answers as $aIndex=>$answer):?>
     <?if($answer['image']):?>
-        <div class="pull-left" style="text-align: center">
+        <div class="quiz-answer-image" style="height: <?=$imgLayerHeight?>px">
             <?=img(array('src'=>'images/data/b/quiz/'.$answer['image']))?>
-            <br>
-            <?=lang_chr($aIndex)?>
+            <div><?=lang_chr($aIndex)?></div>
         </div>
     <?endif?>
 <?endforeach;?>
